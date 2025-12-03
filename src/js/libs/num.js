0ddc10205a53f9c1266bc691e59eb894a52f7876
Cuh4 <89425262+Cuh4@users.noreply.github.com>
2025-12-03 13:49:05 +0000

    feat(js/libs): add `num` lib

---
+++ src/js/libs/num.js
@@ -0,0 +1,33 @@
+/*
+Source-Available No-Redistribution License
+Copyright (c) 2025 cuhHub. All rights reserved.
+
+IMPORTANT: This source code is NOT Open Source.
+
+1. PERMISSIONS (Limited):
+* View and read the source code.
+* Modify for private, non-public use only.
+* Contributions back to the project are permitted.
+
+2. PROHIBITIONS (Strict):
+* You MAY NOT copy, redistribute, publish, or share the code (modified or unmodified).
+* You MAY NOT use this code in ANY public or commercial project or service.
+
+Any violation automatically terminates your rights.
+Provided "AS IS" without warranty.
+Full terms governed by the laws of England and Wales.
+*/
+
+/**
+    Number-related helper functions.
+*/
+export const Num = {}
+
+/**
+    Formats a number (1772 --> 1,772).
+    @param {number} number The number to format
+    @return {number}
+*/
+Num.formatNumber = function(number) {
+    return number.toLocaleString();
+}
