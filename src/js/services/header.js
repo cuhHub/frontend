15a11e1c1bd3f9369125956cad058ed51af96b97
Cuh4 <89425262+Cuh4@users.noreply.github.com>
2026-01-20 01:07:23 +0000

    feat(js/services): add `header.js`. adds `.header-detached` class to header after scrolling past its height

---
+++ src/js/services/header.js
@@ -0,0 +1,57 @@
+/*
+Source-Available No-Redistribution License
+Copyright (c) 2026 cuhHub. All rights reserved.
+
+IMPORTANT: This source code is NOT Open Source.
+
+1. PERMISSIONS (Limited):
+* View and read the source code.
+* Modify for private, non-public use only.
+* Contributions back to the project are permitted.
+
+2. PROHIBITIONS (Strict):
+* You MAY NOT copy, redistribute, publish, or share the code (modified or unmodified).
+* You MAY NOT use this code in ANY public or commercial project or service.
+
+Any violation automatically terminates your rights.
+Provided "AS IS" without warranty.
+Full terms governed by the laws of England and Wales.
+*/
+
+/* -------------- Main */
+
+export const Header = {};
+Header.HEADER_ELEMENT = $("header");
+
+/**
+    Detaches the header
+*/
+Header.detach = async function() {
+    this.HEADER_ELEMENT.addClass("header-detached")
+}
+
+/**
+    Attaches the header
+*/
+Header.attach = async function() {
+    this.HEADER_ELEMENT.removeClass("header-detached")
+}
+
+/**
+    Handles scroll events.
+*/
+Header._scrollHandler = function() {
+    if (window.scrollY > Header.HEADER_ELEMENT.height()) {
+        Header.detach();
+    } else {
+        Header.attach();
+    }
+}
+
+/**
+    Initializes this service.
+*/
+Header.init = function() {
+    this._scrollHandler();
+    document.addEventListener("scroll", this._scrollHandler)
+}
